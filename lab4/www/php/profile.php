9f73cca8d65cefb0d07f6cc99c9ddc497635f091
Akkhawat <630112418060@bru.ac.th>
2022-04-22 12:26:47 +0700

    lab4

---
+++ lab4/www/php/profile.php
@@ -0,0 +1,10 @@
+<?php
+
+echo"Mr.Akkhawat Chatree <br>";
+echo"Nickname : Nuk <br>";
+echo"ID : 630112418060 <br>";
+echo"Address : บุรีรัมย์ <br>";
+echo"work : ทำงานparttimeร้านอาหาร <br>";
+echo"Favourite : youtube,เล่นเกม,ดูหนัง <br>";
+echo"dream career : ข้าราชการ,ยูทูปเบอร์ <br>";
+
